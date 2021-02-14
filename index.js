bf6f711feac0723585ab4e432e1e7675e5d082b4
Evan <evanfujita@gmail.com>
2021-02-14 14:40:38 -0600

    commit tree a46a26bb5833840082ad6ec24c7b27d8e02286b3

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+const map = function(array, func){
+    let newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(func(array[i]))
+    }
+    return newArray
+}
+
+
+const reduce = function(array, func, startingPoint = 0){
+    let total = true
+    
+    for (let i = 0; i < array.length; i++){      
+        if (array[i] === false){
+            total = false
+        }
+    }
+    return total
+}
+
